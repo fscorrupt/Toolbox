cb6c3d45b7b2a10c714ea136de6799c78d1fe046
FSCorrupt <45659314+fscorrupt@users.noreply.github.com>
2023-05-10 14:57:07 +0200

    Update Mass-AudioLangChange.ps1

--- Video/Mass-AudioLangChange.ps1
+++ Video/Mass-AudioLangChange.ps1
@@ -1,6 +1,10 @@
 # Path to the FFmpeg executable
 $ffmpegPath = "C:\ffmpeg\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe"
 
+# Directory containing the MP4 files
+$directory = "N:\VideoFiles"
+
+
 if (!(test-path $ffmpegPath -ErrorAction SilentlyContinue)){
     # Download ffmpeg
     $DownloadUrl = "https://objects.githubusercontent.com/github-production-release-asset-2e65be/292087234/734beec5-c46f-4ee0-9db2-0d8c3eb8c812?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230510%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230510T124639Z&X-Amz-Expires=300&X-Amz-Signature=d23b250963371ef51ac86a2aeaab97d8c113e25044209a6d04fdf3a3fc32b514&X-Amz-SignedHeaders=host&actor_id=45659314&key_id=0&repo_id=292087234&response-content-disposition=attachment%3B%20filename%3Dffmpeg-master-latest-win64-gpl.zip&response-content-type=application%2Foctet-stream"
@@ -15,10 +19,6 @@ if (!(test-path $ffmpegPath -ErrorAction SilentlyContinue)){
 
     Expand-Archive $ZipPath "C:\ffmpeg"
 }
-
-
-# Directory containing the MP4 files
-$directory = "N:\VideoFiles"
 
 # Get all MP4 files in the directory
 $mp4Files = Get-ChildItem -Path $directory -Filter "*.mp4" -File
